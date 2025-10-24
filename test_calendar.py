94a687e98dd301499925a8a86aed1c7663251db2
estelleNdongo <estellendongo02@gmail.com>
2025-10-24 16:13:46 +0200

    test:test_get_chronologic_event

--- test_calendar.py
+++ test_calendar.py
@@ -1,4 +1,5 @@
-from my_calendar import get_events, create_events, get_list_events
+from my_calendar import get_events, create_events, get_list_events, get_chronologic_events
+from datetime import datetime
 
 def test_events_are_in_list():
     events = get_events()
@@ -24,3 +25,10 @@ def test_get_list_events():
         assert isinstance(event[0],str)
         assert isinstance(event[1],int)
         assert isinstance(event[2],str)
+
+def test_get_chronologic_event():
+        event1 = create_events(datetime(2024,1,3,10,0,0),3600,"Event 1")
+        event2 = create_events(datetime(2024,1,1,9,0,0),1800, "Event 2")
+        chronologic_list_events = get_chronologic_events()
+        assert  chronologic_list_events[0] == event2
+        assert chronologic_list_events[1] == event1
