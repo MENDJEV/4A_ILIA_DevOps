ade915ad3f85befa8fdbc4be0d4a0cd0bc0813f2
estelleNdongo <estellendongo02@gmail.com>
2025-10-24 14:49:30 +0200

    test: test_create_events

--- test_calendar.py
+++ test_calendar.py
@@ -1,5 +1,17 @@
-from my_calendar import get_events
+from my_calendar import get_events, create_events
 
 def test_events_are_in_list():
     events = get_events()
     assert isinstance(events, list)
+
+def test_create_events():
+    T = "2024-01-01 10:00:00"
+    t = 3600
+    n = "Réunion"
+    event = create_events(T,t,n)
+    assert isinstance(event,tuple)
+    assert len(event) == 3
+    assert isinstance(event[0],str)
+    assert isinstance(event[1],int)
+    assert isinstance(event[2],str)
+
